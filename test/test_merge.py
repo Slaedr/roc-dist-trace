fccb02d59b6925a5029ed4bf83cff105a73bf91f
Aditya Kashi <kashia@ornl.gov>
2023-10-23 16:24:15 -0400

    5-rank test passes

--- test/test_merge.py
+++ test/test_merge.py
@@ -12,8 +12,9 @@ class MergeTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def compare_traces(self, testdir):
-        inputfiles = [testdir + "/0/results.json", testdir + "/4/results.json", testdir + "/8/results.json"]
+    def compare_traces(self, testdir, ranks):
+        #inputfiles = [testdir + "/0/results.json", testdir + "/4/results.json", testdir + "/8/results.json"]
+        inputfiles = [testdir + "/" + str(rank) + "/results.json" for rank in ranks]
         ref_outputfile = testdir + "/merged.json"
         ref_outdict = {}
         with open(ref_outputfile, 'r') as f:
@@ -23,7 +24,9 @@ class MergeTest(unittest.TestCase):
         outdict = merge_traces(inputfiles)
 
         nevents = len(outdict["traceEvents"])
-        print("No. events/sections in output is " + str(nevents))
+        print("No. events/sections in output is {} and that for ref is {}.".format(
+            nevents, len(ref_outdict["traceEvents"]))
+              )
         self.assertTrue(nevents == len(ref_outdict["traceEvents"]))
         for i in range(nevents):
             if outdict['traceEvents'][i] != ref_outdict["traceEvents"][i]:
@@ -37,7 +40,13 @@ class MergeTest(unittest.TestCase):
         testdir = "./test0"
         if not os.path.isdir(testdir):
             testdir = "./test/test0"
-        self.compare_traces(testdir)
+        self.compare_traces(testdir, [0, 4, 8])
+
+    def test_1(self):
+        testdir = "./test1"
+        if not os.path.isdir(testdir):
+            testdir = "./test/test1"
+        self.compare_traces(testdir, [0, 4, 8, 32, 128])
 
 
 if __name__ == "__main__":
